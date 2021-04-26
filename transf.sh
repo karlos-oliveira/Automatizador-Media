4d117949029633fb9388fe533111089da4ac547f
Carlos Oliveira <karlos.s.oliveira@gmail.com>
2021-04-26 23:30:40 +0100

    Refatoração de codigo; melhorias de performance; ajustes menores

--- transf.sh
+++ transf.sh
@@ -4,94 +4,90 @@
 # e os distribuirem em suas respectivas pastas
 #Autor: Carlos Oliveira
 #Data criação: 15/11/2017
-#Data Atualizaão: 06/10/2019
-#Versão: 2.0
+#Data Atualizaão: 26/4/2021
+#Versão: 2.5
 
 #TODO:
 #
 # 1-) Testar nova versão e descomentar o mover e o escaneamento
-# 2-) Efetuar o tratamento dos erros (mais amigáveis)
-# 3-) Fazer a transferencia de filmes
-# 4-) Aceitar parametros para scanear, trasnf só filmes, só séries, etc
+# 2-) Fazer a transferencia de filmes
 
-_downloadDir="/home/pi/Public/Torrents/Completos"
+helpFunction()
+{
+   echo ""
+   echo "Exemplo de uso: transf.sh -o \"/diretorio/de/origem\" -d \"diretorio/de/destino\" -t 1"
+   echo -e "\t-o Diretorio de origem"
+   echo -e "\t-d Diretorio de destino"
+   echo -e "\t-t Tipo de midia (1-Filme | 2-Serie)"
+   echo -e "\t-s Scanear novas midias(plex) [Opcional | Defaut=false]"
+   echo -e "\t-g Modo debug [Opcional | Default=false]"
+   exit 1 
+}
 
-#_qtdLegendas=$(ls -la *.srt | grep -e "^-"|wc -l)
-#_qtdEpisodios=$(ls -la *.mkv | grep -e "^-"|wc -l)
+while getopts s:t:o:d:g:h: flag
+do
+    case "${flag}" in
+        s) __scannear=${OPTARG};;
+        t) __tipoMedia=${OPTARG};;
+        o) __origemDir=${OPTARG};;
+	d) __destinoDir=${OPTARG};;
+	g) __debug=${OPTARG};;
+	?) helpFunction;;
+    esac
+done
 
-### Criando lista de episódios ###
-ls "$_downloadDir" | grep '.mkv' > listaEpi.tmp
-ls "$_downloadDir" | grep '.mp4' >> listaEpi.tmp
-_qtdEpi=$(cat listaEpi.tmp | wc -l)
+if [ -z "$__origemDir" ] || [ -z "$__destinoDir" ] || [ -z "$__tipoMedia" ]
+then
+   echo "Por favor informe todos os parametros obrigatorios";
+   helpFunction
+fi
 
-### Criando lista de legendas ###
-(ls "$_downloadDir" | grep '.srt') > listaLeg.tmp
-_qtdLeg=$(cat listaLeg.tmp | wc -l)
+### Criando lista de arquivos ###
+ls "$__origemDir" >> listaArquivos.tmp
 
-echo -e "\\nIniciando transferencia de episódios..."
-### Leitura dos  Episodios ###
+echo -e "\\nIniciando transferencia de arquivos..."
+
+### Leitura dos Arquivos ###
 while read f;
 do 
 
-_origem="$_downloadDir/$f"
-
 _arquivo=$(echo "$f" | sed "s/\[eztv\]//");
 _nomeSerie=$(echo "$_arquivo" | grep -oP "(\w+\W)+S\d{1,2}E\d{1,2}");
 _temporada=$(echo "$_nomeSerie" | grep -oP ".S\d{1,2}E\d{1,2}")
 _nomeSerie=$(echo "$_nomeSerie" | sed "s/$_temporada//g"  | sed "s/\./\ /g")
 _temporada=$(echo "$_temporada" | grep -oP "S\d{1,2}" | sed "s/S//g")
-_destino="/home/pi/Multimidia/Séries"
-_nomePasta=$(ls $_destino | grep "$_nomeSerie" | head -1)
-_auxDir=$(echo -e "$_destino/$_nomePasta/$((10#$_temporada))º Temporada")
-_diretorioDes=$(echo "$_auxDir")
-_destino=$(echo "$_diretorioDes/$_arquivo")
+_temporadaDirNome=$(echo "$((10#$_temporada))º Temporada")
+_dirSerie=$(echo -e "$__destinoDir$_nomeSerie")
 
-if [ -d "$_diretorioDes" ]; then
-mv "$_origem" "$_destino"
-
-contEpi=$(($contEpi + 1))
-echo -ne "\\r$contEpi de $_qtdEpi episódio(s) transferidos"
+if [ ! -d "$_dirSerie" ]; then
+mkdir "$_dirSerie"
+chmod 777 "$_dirSerie"
+echo -e "Criado diretório para a série '$_nomeSerie'"
 fi
 
-done < listaEpi.tmp
+_temporadaDir=$(echo -e "$_dirSerie/$_temporadaDirNome")
 
-echo -e "\\n\\nIniciando transferencia de legendas..."
-### Leitura das Legendas ###
-while read f;
-do
-
-_origem="$_downloadDir/$f"
-
-_arquivo=$(echo "$f" | sed "s/\[eztv\]//");
-_nomeSerie=$(echo "$_arquivo" | grep -oP "(\w+\W)+S\d{1,2}E\d{1,2}");
-_temporada=$(echo "$_nomeSerie" | grep -oP ".S\d{1,2}E\d{1,2}")
-_nomeSerie=$(echo "$_nomeSerie" | sed "s/$_temporada//g"  | sed "s/\./\ /g")
-_temporada=$(echo "$_temporada" | grep -oP "S\d{1,2}" | sed "s/S//g")
-_destino="/home/pi/Multimidia/Séries"
-_nomePasta=$(ls $_destino | grep "$_nomeSerie" | head -1)
-_auxDir=$(echo "$_destino/$_nomePasta/$((10#$_temporada))º Temporada")
-_diretorioDes=$(echo "$_auxDir")
-_destino=$(echo "$_diretorioDes/$_arquivo")
-
-if [ -d "$_diretorioDes" ]; then
-mv "$_origem" "$_destino"
-
-contLeg=$(($contLeg + 1))
-echo -ne "\\r$contLeg de $_qtdLeg legenda(s) transferidas"
+if [ ! -d "$_temporadaDir" ]; then
+mkdir "$_temporadaDir"
+chmod 777 "$_temporadaDir"
+echo -e "Criado o diretório para a $_temporadaDirNome da série '$_nomeSerie'"
 fi
 
-done < listaLeg.tmp
+cp "$__origemDir$f" "$_temporadaDir$arquivo"
 
+echo -e "$_arquivo copiado com sucesso"
+
+done < listaArquivos.tmp
 echo -e "\\n"
 
 ### Removendo Arquivos Temporarios ###
-rm listaEpi.tmp
-rm listaLeg.tmp
+rm listaArquivos.tmp
 
 
 ### Efetuando o Scan das Bibliotecas ###
-
+#if [ -z $__scannear ]; then
 #export LD_LIBRARY_PATH=/usr/lib/plexmediaserver
 
 #/usr/lib/plexmediaserver/Plex\ Media\ Scanner --scan --refresh --section 6
+#fi
 
